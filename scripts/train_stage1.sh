ce3e87ac4b06342d701e33e2552a798f532f7ffe
hao405 <1815526897@qq.com>
2026-03-19 10:55:56 +0800

    Add data with LFS

--- scripts/train_stage1.sh
+++ scripts/train_stage1.sh
@@ -42,14 +42,14 @@ torchrun --standalone --nproc_per_node=8 "${TRAIN_RUN}" \
     --wandb_mode online \
     --device cuda \
     --dtype float32 \
-    --np_seed 42 \
+    --np_seed 43 \
     --torch_seed 42 \
-    --max_steps 100000 \
+    --max_steps 160000 \
     --batch_size 512 \
     --micro_batch_size 4 \
-    --lr 1e-4 \
+    --lr 5e-5 \
     --scheduler cosine_warmup \
-    --warmup_proportion 0.02 \
+    --warmup_proportion 0.05 \
     --gradient_clipping 1.0 \
     --prior_type mix_scm \
     --prior_device cpu \
@@ -57,7 +57,7 @@ torchrun --standalone --nproc_per_node=8 "${TRAIN_RUN}" \
     --min_features 2 \
     --max_features 100 \
     --max_classes 10 \
-    --max_seq_len 1024 \
+    --max_seq_len 2048 \
     --min_train_size 0.1 \
     --max_train_size 0.9 \
     --embed_dim 128 \
@@ -75,66 +75,66 @@ torchrun --standalone --nproc_per_node=8 "${TRAIN_RUN}" \
     --checkpoint_dir "$STAGE1_CHECKPOINT_DIR" \
     --save_temp_every 50 \
     --save_perm_every 5000
+    --only_load_model True \
 
+# # ------------------------------------------------------
+# # Save prior datasets to disk and load them for training
+# # ------------------------------------------------------
 
-# ------------------------------------------------------
-# Save prior datasets to disk and load them for training
-# ------------------------------------------------------
+# # Saving to disk
+# python "${PRIOR_GENLOAD}" \
+#     --save_dir "$STAGE1_PRIOR_DIR" \
+#     --np_seed 43 \
+#     --torch_seed 42 \
+#     --num_batches 100000 \
+#     --resume_from 0 \
+#     --batch_size 512 \
+#     --batch_size_per_gp 4 \
+#     --prior_type mix_scm \
+#     --min_features 2 \
+#     --max_features 100 \
+#     --max_classes 10 \
+#     --max_seq_len 2048 \
+#     --min_train_size 0.1 \
+#     --max_train_size 0.9 \
+#     --n_jobs -1 \
+#     --num_threads_per_generate 1 \
+#     --device cpu
 
-# Saving to disk
-python "${PRIOR_GENLOAD}" \
-    --save_dir "$STAGE1_PRIOR_DIR" \
-    --np_seed 42 \
-    --torch_seed 42 \
-    --num_batches 100000 \
-    --resume_from 0 \
-    --batch_size 512 \
-    --batch_size_per_gp 4 \
-    --prior_type mix_scm \
-    --min_features 2 \
-    --max_features 100 \
-    --max_classes 10 \
-    --max_seq_len 1024 \
-    --min_train_size 0.1 \
-    --max_train_size 0.9 \
-    --n_jobs -1 \
-    --num_threads_per_generate 1 \
-    --device cpu
-
-# Loading from disk and training
-torchrun --standalone --nproc_per_node=8 "${TRAIN_RUN}" \
-    --wandb_log True \
-    --wandb_project TabICL \
-    --wandb_name Stage1 \
-    --wandb_dir "$WANDB_DIR" \
-    --wandb_mode online \
-    --device cuda \
-    --dtype float32 \
-    --np_seed 42 \
-    --torch_seed 42 \
-    --max_steps 100000 \
-    --batch_size 512 \
-    --micro_batch_size 4 \
-    --lr 1e-4 \
-    --scheduler cosine_warmup \
-    --warmup_proportion 0.02 \
-    --gradient_clipping 1.0 \
-    --prior_dir "$STAGE1_PRIOR_DIR" \
-    --load_prior_start 0 \
-    --delete_after_load False \
-    --prior_device cpu \
-    --embed_dim 128 \
-    --col_num_blocks 3 \
-    --col_nhead 4 \
-    --col_num_inds 128 \
-    --row_num_blocks 3 \
-    --row_nhead 8 \
-    --row_num_cls 4 \
-    --row_rope_base 100000 \
-    --icl_num_blocks 12 \
-    --icl_nhead 4 \
-    --ff_factor 2 \
-    --norm_first True \
-    --checkpoint_dir "$STAGE1_CHECKPOINT_DIR" \
-    --save_temp_every 50 \
-    --save_perm_every 5000
+# # Loading from disk and training
+# torchrun --standalone --nproc_per_node=8 "${TRAIN_RUN}" \
+#     --wandb_log True \
+#     --wandb_project TabICL \
+#     --wandb_name Stage1 \
+#     --wandb_dir "$WANDB_DIR" \
+#     --wandb_mode online \
+#     --device cuda \
+#     --dtype float32 \
+#     --np_seed 42 \
+#     --torch_seed 42 \
+#     --max_steps 100000 \
+#     --batch_size 512 \
+#     --micro_batch_size 4 \
+#     --lr 1e-4 \
+#     --scheduler cosine_warmup \
+#     --warmup_proportion 0.02 \
+#     --gradient_clipping 1.0 \
+#     --prior_dir "$STAGE1_PRIOR_DIR" \
+#     --load_prior_start 0 \
+#     --delete_after_load False \
+#     --prior_device cpu \
+#     --embed_dim 128 \
+#     --col_num_blocks 3 \
+#     --col_nhead 4 \
+#     --col_num_inds 128 \
+#     --row_num_blocks 3 \
+#     --row_nhead 8 \
+#     --row_num_cls 4 \
+#     --row_rope_base 100000 \
+#     --icl_num_blocks 12 \
+#     --icl_nhead 4 \
+#     --ff_factor 2 \
+#     --norm_first True \
+#     --checkpoint_dir "$STAGE1_CHECKPOINT_DIR" \
+#     --save_temp_every 50 \
+#     --save_perm_every 5000
